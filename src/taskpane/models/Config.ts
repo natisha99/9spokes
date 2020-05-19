ae81af72813359fcc42cf02b979fce4b4038343e
Nik Ponomarov <nosurlife@hotmail.com>
2020-05-20 03:16:32 +1200

    Add functionality for adding and removing config values

--- src/taskpane/models/Config.ts
+++ src/taskpane/models/Config.ts
@@ -1,6 +1,23 @@
 export interface Config {
-  house: { companyNumber: string }[];
-  linkedin: {}[];
-  finance: { ticker: string; interval: string; range: string }[];
-  trends: { keyword: string; weeks: number }[];
+  house: HouseConfig[];
+  linkedin: LinkedinConfig[];
+  finance: FinanceConfig[];
+  trends: TrendsConfig[];
+}
+
+export interface HouseConfig {
+  companyNumber: string;
+}
+
+export interface LinkedinConfig {}
+
+export interface FinanceConfig {
+  ticker: string;
+  interval: string;
+  range: string;
+}
+
+export interface TrendsConfig {
+  keyword: string;
+  weeks: number;
 }
